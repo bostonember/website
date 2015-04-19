adae36eaaf5eb9c44a46457a1ab122aa54c9abab
Chris Ball <chris@echobind.com>
2015-04-19 14:29:01 -0400

    Add modal to add speakers during meetup create.

--- app/pods/admin/meetups/new/route.js
+++ app/pods/admin/meetups/new/route.js
@@ -1,16 +1,10 @@
 import Ember from 'ember';
 import AdminAuthenticatedRoute from 'boston-ember/mixins/admin-authenticated-route';
-var { moment } = window;
 
 export default Ember.Route.extend(AdminAuthenticatedRoute, {
   model() {
-    var newMeetup = this.store.createRecord('meetup', { date: new Date()});
-    var speakers = this.store.findAll('speaker');
-    var presentations = [
-      this.store.createRecord('presentation'),
-      this.store.createRecord('presentation')
-    ];
-    newMeetup.get('presentations').pushObjects(presentations);
+    var speakers = this._allPersistedSpeakers();
+    var newMeetup = this._newMeetupWithPresentationsAndSpeakers();
 
     return Ember.RSVP.hash({
       meetup: newMeetup,
@@ -18,7 +12,28 @@ export default Ember.Route.extend(AdminAuthenticatedRoute, {
     });
   },
 
+  /**
+    When this route exits, delete any newly created meetup/presentation records.
+  */
+  resetController(controller, isExiting) {
+    var meetup = controller.get('model.meetup');
+
+    if (isExiting && meetup.get('isNew')) {
+      meetup.get('presentations').then((presentations) => {
+        presentations.forEach((presentation) => {
+          presentation.destroyRecord();
+        });
+      });
+
+      meetup.destroyRecord();
+    }
+  },
+
   actions: {
+
+    /**
+      Saves a Meetup along with child presentation and speaker records.
+    */
     saveMeetup(meetup) {
       var presentations = meetup.get('presentations');
       var actualDate = meetup.get('date').toDate();
@@ -43,5 +58,34 @@ export default Ember.Route.extend(AdminAuthenticatedRoute, {
         });
       });
     }
+  },
+
+  /**
+    Fetches all speakers, then returns a live RecordArray of persisted speakers.
+  */
+  _allPersistedSpeakers() {
+    return this.store.findAll('speaker').then(() => {
+      return this.store.filter('speaker', function(speaker) {
+        return !speaker.get('isNew');
+      });
+    });
+  },
+
+  /**
+    Creates a new Meetup record with new presentations and speakers.
+  */
+  _newMeetupWithPresentationsAndSpeakers() {
+    var newMeetup = this.store.createRecord('meetup', { date: new Date()});
+
+    var newSpeaker1 = this.store.createRecord('speaker');
+    var newSpeaker2 = this.store.createRecord('speaker');
+    var presentations = [
+      this.store.createRecord('presentation', { speaker: newSpeaker1 }),
+      this.store.createRecord('presentation', { speaker: newSpeaker2 })
+    ];
+
+    newMeetup.get('presentations').pushObjects(presentations);
+
+    return newMeetup;
   }
 });
--- app/pods/components/presentation-form/component.js
+++ app/pods/components/presentation-form/component.js
@@ -1,10 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isAddingSpeaker: false,
+  selectedSpeaker: null,
+
   actions: {
+    addSpeaker(speaker) {
+      speaker.save().then(() => {
+        this.set('selectedSpeaker', speaker);
+        this.set('isAddingSpeaker', false);
+      });
+    },
+
     setSpeaker(speaker) {
       var presentation = this.get('presentation');
       presentation.set('speaker', speaker);
+    },
+
+    showAddSpeakerModal() {
+      this.set('isAddingSpeaker', true);
+    },
+
+    dismissAddSpeakerModal() {
+      this.set('isAddingSpeaker', false);
     }
   }
 });
