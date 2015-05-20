d314f4351edb4d0583d4cda4fc20495553dfe69a
Chris Ball <chris@echobind.com>
2015-05-19 22:22:44 -0400

    Add format markdown helper.

---
+++ tests/unit/helpers/format-markdown-test.js
@@ -0,0 +1,12 @@
+import {
+  formatMarkdown
+} from '../../../helpers/format-markdown';
+import { module, test } from 'qunit';
+
+module('FormatMarkdownHelper');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = formatMarkdown(42);
+  assert.ok(result);
+});
