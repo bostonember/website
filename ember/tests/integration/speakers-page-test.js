f8b8219c84c942ca6a573548294df04b1dfadf43
Michal <michalzagrodzki@gmail.com>
2015-05-28 21:38:08 +0200

    Correct speakers test.
    Add missing 'assert' input value, so the data can be sent along the test.

--- ember/tests/integration/speakers-page-test.js
+++ ember/tests/integration/speakers-page-test.js
@@ -49,39 +49,39 @@ module('Integration - Speaker Page', {
   }
 });
 
-test('Should allow navigation to the speakers page from the landing page', function() {
+test('Should allow navigation to the speakers page from the landing page', function(assert) {
   visit('/').then(function() {
     click('a:contains("Speakers")').then(function() {
-      equal(find('h3').text(), 'Speakers');
+      assert.equal(find('h3').text(), 'Speakers');
     });
   });
 });
 
-test('Should list all speakers and number of presentations', function() {
+test('Should list all speakers and number of presentations', function(assert) {
   visit('/speakers').then(function() {
-    equal(find('a:contains("Bugs Bunny (2)")').length, 1);
-    equal(find('a:contains("Wile E. Coyote (1)")').length, 1);
-    equal(find('a:contains("Yosemite Sam (3)")').length, 1);
+    assert.equal(find('a:contains("Bugs Bunny (2)")').length, 1);
+    assert.equal(find('a:contains("Wile E. Coyote (1)")').length, 1);
+    assert.equal(find('a:contains("Yosemite Sam (3)")').length, 1);
   });
 });
 
-test('Should be able to navigate to a speaker page', function() {
+test('Should be able to navigate to a speaker page', function(assert) {
   visit('/speakers').then(function() {
     click('a:contains("Bugs Bunny")').then(function() {
-      equal(find('h4').text(), 'Bugs Bunny');
+      assert.equal(find('h4').text(), 'Bugs Bunny');
     });
   });
 });
 
-test('Should be able to visit a speaker page', function() {
+test('Should be able to visit a speaker page', function(assert) {
   visit('/speakers/1').then(function() {
-    equal(find('h4').text(), 'Bugs Bunny');
+    assert.equal(find('h4').text(), 'Bugs Bunny');
   });
 });
 
-test('Should list all presentations for a speaker', function() {
+test('Should list all presentations for a speaker', function(assert) {
   visit('/speakers/1').then(function() {
-    equal(find('li:contains("What\'s up with Docs?")').length, 1);
-    equal(find('li:contains("Of course, you know, this means war.")').length, 1);
+    assert.equal(find('li:contains("What\'s up with Docs?")').length, 1);
+    assert.equal(find('li:contains("Of course, you know, this means war.")').length, 1);
   });
 });
